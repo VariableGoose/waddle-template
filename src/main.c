ec052d6af36c16d2cc1ca0ed1a01026c8e7421c5
Linus Kåreblom <linus.kareblom@gmail.com>
2025-01-07 19:16:08 +0100

    Replace printf with waddle logging functions and change CMake to use relative paths for __FILE__ macro

--- src/main.c
+++ src/main.c
@@ -1,19 +1,17 @@
 #define WADDLE_IMPLEMENTATION
 #include "waddle.h"
 
-#include <stdio.h>
-
 i32 main(void) {
     wdl_init();
     WDL_Arena* arena = wdl_arena_create();
 
     wdl_arena_push(arena, wdl_os_get_page_size());
 
-    printf("Waddle template!\n");
-    printf("Page size: %u\n", wdl_os_get_page_size());
-    printf("Architecture: %lu-bit\n", sizeof(void*) * 8);
-    printf("Arena pos: %llu\n", wdl_arena_get_pos(arena));
-    printf("Run time: %f\n", wdl_os_get_time());
+    WDL_INFO("Waddle template!");
+    WDL_INFO("Page size: %u", wdl_os_get_page_size());
+    WDL_INFO("Architecture: %lu-bit", sizeof(void*) * 8);
+    WDL_INFO("Arena pos: %llu", wdl_arena_get_pos(arena));
+    WDL_INFO("Run time: %f", wdl_os_get_time());
 
     wdl_arena_destroy(arena);
     wdl_terminate();
