3e05004ad8cf8bb6234b7f181fbcf9e7f45bcfda
Linus Kåreblom <linus.kareblom@gmail.com>
2025-01-16 19:36:14 +0100

    Update to new macro function naming convention.

--- src/main.c
+++ src/main.c
@@ -7,11 +7,11 @@ i32 main(void) {
 
     wdl_arena_push(arena, wdl_os_get_page_size());
 
-    WDL_INFO("Waddle template!");
-    WDL_INFO("Page size: %u", wdl_os_get_page_size());
-    WDL_INFO("Architecture: %lu-bit", sizeof(void*) * 8);
-    WDL_INFO("Arena pos: %llu", wdl_arena_get_pos(arena));
-    WDL_INFO("Run time: %f", wdl_os_get_time());
+    wdl_info("Waddle template!");
+    wdl_info("Page size: %u", wdl_os_get_page_size());
+    wdl_info("Architecture: %lu-bit", sizeof(void*) * 8);
+    wdl_info("Arena pos: %llu", wdl_arena_get_pos(arena));
+    wdl_info("Run time: %f", wdl_os_get_time());
 
     wdl_arena_destroy(arena);
     wdl_terminate();
